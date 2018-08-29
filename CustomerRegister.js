132ee1cbbec649cbe106455472b609e78484de75
Jerzy Wickowski <kontakt@jerzywickowski.pl>
2018-08-29 13:56:05 +0200

    pass test to min pass length

--- CustomerRegister.js
+++ CustomerRegister.js
@@ -1,10 +1,15 @@
 function CustomerRegister(){}
 
 CustomerRegister.register = function register(login, password){
+var dirtyHardcodedMinPasswordLength = 10;
 if(!login){
     throw "Login is required";
 }
 if(!password){
     throw "Password is required"
+}
+
+if(password.length < dirtyHardcodedMinPasswordLength){
+    throw "Password length should be at least " + dirtyHardcodedMinPasswordLength;
 }
 }
