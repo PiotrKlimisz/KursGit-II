6336b61e943a50487e94f17c95a6f5b11f335d13
Jerzy Wickowski <kontakt@jerzywickowski.pl>
2018-08-29 13:32:33 +0200

    add test Password is required

--- CustomerRegister.test.js
+++ CustomerRegister.test.js
@@ -4,12 +4,23 @@ if(CustomerRegister){
 
 var customerRegister = Object.create(CustomerRegister);
 
-try{
+
+try {
     customerRegister.register();
     console.log("FAIL: login is required");
 } 
 catch(error){
     if(error !== "Login is required"){
         console.log("FAIL: Expect 'Login is required' message");
+    }
+}
+
+try {
+    customElements.register("sampleLogin", "");
+    console.log("FAIL: password is required")
+}
+catch(error){
+    if(error !== "Password is required"){
+        console.log("FAIL: Expect 'Password is required'");
     }
 }
