f0e64541845cf2ba0bb3cf5c97e367045587001a
Jerzy Wickowski <kontakt@jerzywickowski.pl>
2018-08-29 13:49:53 +0200

    add test: Password lenght should be at least 9

--- CustomerRegister.test.js
+++ CustomerRegister.test.js
@@ -24,3 +24,13 @@ catch(error){
         console.log("FAIL: Expect 'Password is required'");
     }
 }
+
+try {
+    customerRegister.register("sampleLogin", "shortPass");
+    console.log("FAIL: Password lenght should be at least 9");
+}
+catch(error){
+    if(error !== "Password lenght should be at least 9"){
+        console.log("FAIL: Expect 'Password lenght should be at least 9'");
+    }
+}
